56ec7baac76fc1aae0f3f4044dcb9478ffd2a3cd
Pritesh-movinture <pritesh.kale@movinture.com>
2023-03-07 18:08:18 +0530

    Fomatted the code.

--- python training/sidebar_menu_data_scrapped.py
+++ python training/sidebar_menu_data_scrapped.py
@@ -3,27 +3,23 @@ from bs4 import BeautifulSoup
 import json
 
 def json_from_html_using_bs4(base_url):
+    res = ''
+    awsPage = requests.get(base_url)
 
-    page = requests.get(base_url)
-
-    soup = BeautifulSoup(page.text, "html.parser")
+    soup = BeautifulSoup(awsPage.text, "html.parser")
 
     faqs = soup.find_all(
         'a', attrs={'class':
             'lb-txt-bold lb-txt-none lb-txt-16 lb-txt'})
-    # print(faqs)
+    
 
+# Here we get anchor tag data of side section
     for faq in faqs:
         print(faq.getText())
         
-    #     res.append(title)
-    #     div += 1
-    # return res
-
-    data = '''faq'''
+    data = {"section" : faq.getText()} # converting side section into json
 
     faqs.append(data)
-    # title += 1
     return res
 
 if __name__=="__main__":
@@ -36,14 +32,3 @@ if __name__=="__main__":
         json.dump(sidebarMenuJSON, ensure_ascii=False)
     print("Created JSON file")
 
-# base_url = "https://aws.amazon.com/ec2/faqs/"
-
-# r = requests.get(base_url)
-# htmlContent = r.content
-
-# soup = BeautifulSoup(htmlContent,'html.parser')
-
-# title = soup.title
-
-# paras = soup.find('h2')
-# print (paras.prettify())
