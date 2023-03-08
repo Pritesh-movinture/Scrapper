f2605ce194e75f70b36a600c73d8119bf5a526cc
Pritesh-movinture <pritesh.kale@movinture.com>
2023-03-08 18:17:23 +0530

    Brought questions in JSON format

--- python training/sidebar_menu_data_scrapped.py
+++ python training/sidebar_menu_data_scrapped.py
@@ -3,9 +3,12 @@ from bs4 import BeautifulSoup
 import json
 
 def json_from_html_using_bs4(base_url):
-    res = ''
+    res = []
+    dataNew = []
+    dataNewQuestion = []
     awsPage = requests.get(base_url)
 
+    
     soup = BeautifulSoup(awsPage.text, "html.parser")
 
     faqs = soup.find_all(
@@ -14,12 +17,38 @@ def json_from_html_using_bs4(base_url):
     
 
 # Here we get anchor tag data of side section
-    for faq in faqs:
-        print(faq.getText())
+    # for faq in faqs:
+    #     print(faq.getText())
         
-    data = {"section" : faq.getText()} # converting side section into json
+    # data = {'section' : faq.getText()} # converting side section into json
 
+    questions = soup.find_all(
+        'div',attrs={'class':'lb-txt-16 lb-rtxt'}
+    )
+
+    for question in questions:
+        # print(question.getText())
+        dataNew.append(question.getText())
+        # i = question.getText()
+        # print(type(i))
+        data = {'subsection': question.getText()}
+        # print (data) 
+        
+        # for data in questions:
+        #     print(data[0])     
+    # print(dataNew)   
+    
+    for newData in dataNew:
+        type = newData.split("\n")
+        x = type[0]
+        y= type[1]
+        print(y)
+        dataObj={"question":type[1]}
+        dataNewQuestion.append(dataObj)
     faqs.append(data)
+    res.append(dataNewQuestion)
+
+    
     return res
 
 if __name__=="__main__":
@@ -27,8 +56,7 @@ if __name__=="__main__":
     base_url = "https://aws.amazon.com/ec2/faqs/"
 
     res = json_from_html_using_bs4(base_url)
-
-    with open(res,'sidebar_menu_data_scrapped.json','w') as sidebarMenuJSON:
-        json.dump(sidebarMenuJSON, ensure_ascii=False)
-    print("Created JSON file")
-
+    # print(res)
+    with open('question-answer.json', 'w', encoding='utf8') as f:
+        json.dump(res, f, indent=8, ensure_ascii=False)
+    print("Created Json File")
